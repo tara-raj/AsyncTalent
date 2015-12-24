069ef4116e0ae733b111de1c8c33b8ccd60fc3c4
Tara R <tlr8ed@virginia.edu>
2015-12-24 16:57:24 -0500

    Countdown Timer
    Standalone countdown timer

---
+++ pages/timer/timer.php
@@ -0,0 +1,116 @@
+<?php
+ //session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <script src="../js/star-rating.js" type="text/javascript"></script>
+
+    <title>EmployED - Find Companies</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<?php
+//session_start();
+$timestamp = time();
+$diff = 10; //<-Time of countdown in seconds.  ie. 3600 = 1 hr. or 86400 = 1 day.
+
+//MODIFICATION BELOW THIS LINE IS NOT REQUIRED.
+$hld_diff = $diff;
+if(isset($_SESSION['ts'])) {
+	$slice = ($timestamp - $_SESSION['ts']);	
+	$diff = $diff - $slice;
+}
+
+if(!isset($_SESSION['ts']) || $diff > $hld_diff || $diff < 0) {
+	$diff = $hld_diff;
+	$_SESSION['ts'] = $timestamp;
+}
+
+//Below is demonstration of output.  Seconds could be passed to Javascript.
+$diff; //$diff holds seconds less than 3600 (1 hour);
+
+$hours = floor($diff / 3600) . ' : ';
+$diff = $diff % 3600;
+$minutes = floor($diff / 60) . ' : ';
+$diff = $diff % 60;
+$seconds = $diff;
+
+
+?>
+<div id="strclock">Clock Here!</div>
+<script type="text/javascript">
+ var hour = <?php echo floor($hours); ?>;
+ var min = <?php echo floor($minutes); ?>;
+ var sec = <?php echo floor($seconds); ?>
+
+function countdown() {
+ if(sec <= 0 && min > 0) {
+  sec = 59;
+  min -= 1;
+ }
+ else if(min <= 0 && sec <= 0) {
+  min = 0;
+  sec = 0;
+ }
+ else {
+  sec -= 1;
+ }
+ 
+ if(min <= 0 && hour > 0) {
+  min = 59;
+  hour -= 1;
+ }
+ 
+ var pat = /^[0-9]{1}$/;
+ sec = (pat.test(sec) == true) ? '0'+sec : sec;
+ min = (pat.test(min) == true) ? '0'+min : min;
+ hour = (pat.test(hour) == true) ? '0'+hour : hour;
+ 
+ document.getElementById('strclock').innerHTML = hour+":"+min+":"+sec;
+ setTimeout("countdown()",1000);
+ 
+ var tt = document.getElementById('strclock').innerHTML
+ if(sec <= "15"){
+ 	//document.getElementById('strclock').innerHTML = "Time's Up!";
+ 	document.getElementById('strclock').style.color = "red";
+ 	document.getElementById("strclock").style.font = "bold 20px arial";
+ }
+ if( sec == "00"){
+ 	document.getElementById('strclock').innerHTML = "Time's Up!";
+ }
+ 
+ }
+ countdown();
+ 
+</script>
+</body>
+
+
+</html>
